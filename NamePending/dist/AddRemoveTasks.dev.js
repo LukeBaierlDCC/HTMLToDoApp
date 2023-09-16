b3cb0e5ba14f7aa40c385c412e40b3ae265be825
LUKE BAIERL <lukeest1984@gmail.com>
2023-09-16 16:01:59 -0500

    added user story details to BusRoute and TruckTrax

---
+++ NamePending/dist/AddRemoveTasks.dev.js
@@ -0,0 +1,54 @@
+"use strict";
+
+window.onload = function () {
+  var savedTasks = localStorage.getItem('tasks');
+
+  if (savedTasks) {
+    var taskList = document.getElementById('taskList');
+    taskList.innerHTML = savedTasks;
+    attachDeleteListeners();
+  }
+};
+
+function addTask() {
+  var taskInput = document.getElementById('taskInput');
+  var taskText = taskInput.value.trim();
+
+  if (taskText === '') {
+    alert('Please enter a task!');
+    return;
+  }
+
+  var taskList = document.getElementById('taskList');
+  var listItem = document.createElement('li');
+  listItem.textContent = taskText;
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  deleteButton.onclick = function () {
+    taskList.removeChild(listItem);
+    updateLocalStorage();
+  };
+
+  listItem.appendChild(deleteButton);
+  taskList.appendChild(listItem);
+  taskInput.value = '';
+  updateLocalStorage();
+  attachDeleteListeners();
+}
+
+function updateLocalStorage() {
+  var taskList = document.getElementById('taskList');
+  localStorage.setItem('tasks', taskList.innerHTML);
+}
+
+function attachDeleteListeners() {
+  var deleteButtons = document.querySelectorAll('#taskList li button');
+  deleteButtons.forEach(function (button) {
+    button.onclick = function () {
+      button.parentElement.remove();
+      updateLocalStorage();
+    };
+  });
+}
+//# sourceMappingURL=AddRemoveTasks.dev.js.map
